518c759c7b57336fa3a423b26781bd8e4d6e1ba2
Kami-969 <64987182+Kami-969@users.noreply.github.com>
2020-06-08 02:01:59 +0400

    done

---
+++ HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string print(string name, int Age)
+        {
+            return $"Name : {name} Age = {Age}";
+        }
+
+        public string objprint(Users users)
+        {
+            return $"Age : {users.id} Name = {users.user}";
+        }
+        
+        public ActionResult Anon ()
+        {
+            var student = new { First = "Kami", Last = "Yakimas" };
+
+            return Json(student, JsonRequestBehavior.AllowGet);
+
+
+        }
+
+        
+    }
+   
+  
+}
